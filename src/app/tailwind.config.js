51f13a4dba0e873ab252526639bb5023fe36c45a
yukthika Fernando <yukthika.fernando@churchillcap.com>
2023-10-21 15:59:26 +0530

    commit tailwind config

---
+++ src/app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'], // or specify the paths to all of your HTML and TypeScript files
+  // ... other configurations
+}
+
